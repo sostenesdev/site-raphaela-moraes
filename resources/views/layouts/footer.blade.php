b1e285eb9381b36751d2bbc677eb97086b19286b
sdosistemas <sdosistemasbr@gmail.com>
2023-07-12 01:01:07 -0300

    Melhorias no footer e na sidebar

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,13 +3,13 @@
         <div class="row text-muted">
             <div class="col-6 text-start">
                 <p class="mb-0">
-                    <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>AdminKit</strong></a> - <a class="text-muted" href="https://adminkit.io/" target="_blank"><strong>Bootstrap Admin Template</strong></a>								&copy;
+                    <a class="text-muted" href="" target="_blank"><strong>SDO Sistemas</strong></a> - <a class="text-muted" href="" target="_blank"><strong>Painel Administrativo</strong></a>&copy; {{date('Y')}} - Todos os Direitos Reservados
                 </p>
             </div>
-            <div class="col-6 text-end">
+            {{-- <div class="col-6 text-end">
                 <ul class="list-inline">
                     <li class="list-inline-item">
-                        <a class="text-muted" href="https://adminkit.io/" target="_blank">Support</a>
+                        <a class="text-muted" href="#" target="_blank">Suporte</a>
                     </li>
                     <li class="list-inline-item">
                         <a class="text-muted" href="https://adminkit.io/" target="_blank">Help Center</a>
@@ -21,7 +21,7 @@
                         <a class="text-muted" href="https://adminkit.io/" target="_blank">Terms</a>
                     </li>
                 </ul>
-            </div>
+            </div> --}}
         </div>
     </div>
 </footer>
