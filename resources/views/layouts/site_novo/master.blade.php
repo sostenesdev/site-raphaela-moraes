206ee3223da66b8b90bbb1f50c0eaac0992c524b
sdosistemas <sdosistemasbr@gmail.com>
2023-07-12 19:28:29 -0300

    adicionando exibição de imagem na tela inicial

--- resources/views/layouts/site_novo/master.blade.php
+++ resources/views/layouts/site_novo/master.blade.php
@@ -43,7 +43,7 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo me-auto"><a href="index.html">Raphaela Moraes</a></h1>
+      <h1 class="logo me-auto"><a href="index.html">{{$model->titulo}}</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -89,12 +89,12 @@
           <h1>{{$model->titulo}}</h1>
           <h2>{{$model->subtitulo}}</h2>
           <div class="d-flex justify-content-center justify-content-lg-start">
-            <a href="#about" class="btn-get-started scrollto">Get Started</a>
-            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a>
+            {{-- <a href="#about" class="btn-get-started scrollto">Get Started</a> --}}
+            {{-- <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox btn-watch-video"><i class="bi bi-play-circle"></i><span>Watch Video</span></a> --}}
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="{{ asset('assets/site_novo/img/hero-img.png')}}" class="img-fluid animated" alt="">
+          <img src="{{ isset($model->imagem_principal)? $model->getImage() : asset('assets/site_novo/img/hero-img.png')}}" class="img-fluid animated" alt="">
         </div>
       </div>
     </div>
