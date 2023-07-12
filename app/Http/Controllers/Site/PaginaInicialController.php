6c56fd2968d58f5904749be430f0e980fe75cece
sdosistemas <sdosistemasbr@gmail.com>
2023-07-12 18:40:59 -0300

    formulario para cadastramento da página inicial

--- app/Http/Controllers/Site/PaginaInicialController.php
+++ app/Http/Controllers/Site/PaginaInicialController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\PaginaInicial;
 
 class PaginaInicialController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $paginaInicial = PaginaInicial::orderBy('id', 'desc')->first();
+        $paginaInicial = $paginaInicial == null ? new PaginaInicial() : $paginaInicial;
+        return view('welcome',['model' => $paginaInicial]);
     }
 }
