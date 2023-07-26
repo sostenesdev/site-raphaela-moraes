aa1aa3934dcc20db3d20ffe3d0f0a83aeefd8457
sdosistemas <sdosistemasbr@gmail.com>
2023-07-26 12:46:11 -0300

    Mais melhorias na exibiçaõ dos projetos

--- resources/views/site_v2/proposicoes.blade.php
+++ resources/views/site_v2/proposicoes.blade.php
@@ -29,22 +29,31 @@
                 </div>
                 <div class="card-body">
                 <div class="row">
-                    <table class="table">
-                        <tr>
-                            <td><strong>Protocolo:</strong></td>
-                            <td>{{$p->protocolo}}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Processo:</strong></td>
-                            <td>{{$p->processo}}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Data:</strong></td>
-                            <td>{{$p->data}}</td>
-                        </tr>
-                    </table>
-                </div>
+                    <div class="col col-1">
+                    <strong>Protocolo:</strong>
+                    </div>
+                    <div class="col col-10 text-left">
+                        {{$p->protocolo}}
+                    </div>
+                </div><hr/>
                 <div class="row">
+                    <div class="col col-1">
+                    <strong>Processo:</strong>
+                    </div>
+                    <div class="col col-10 text-left">
+                        {{$p->processo}}
+                    </div>
+                </div><hr/>
+                <div class="row">
+                    <div class="col col-1">
+                    <strong>Data:</strong>
+                    </div>
+                    <div class="col col-10 text-left">
+                        {{(new DateTime($p->data))->format('d/m/Y')}}
+                    </div>
+                </div><hr/>
+                </div>
+                <div class="row mb-5">
                     <div class="col col-12 text-center">
                         <a class="btn btn-primary" href="{{$p->descricao}}">Saiba Mais</a>
                     </div>
