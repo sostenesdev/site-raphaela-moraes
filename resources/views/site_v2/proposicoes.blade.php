8e9065477aea7475783bbc2effad23ca08db03ac
sdosistemas <sdosistemasbr@gmail.com>
2023-07-26 12:52:31 -0300

    teste

--- resources/views/site_v2/proposicoes.blade.php
+++ resources/views/site_v2/proposicoes.blade.php
@@ -30,26 +30,26 @@
                 </div>
                 <div class="card-body">
                 <div class="row">
-                    <div class="col col-2">
+                    <div class="col col-1">
                     <strong>Protocolo:</strong>
                     </div>
-                    <div class="col col-10 text-left">
+                    <div class="col col-11 text-left">
                         {{$p->protocolo}}
                     </div>
                 </div><hr/>
                 <div class="row">
-                    <div class="col col-2">
+                    <div class="col col-1">
                     <strong>Processo:</strong>
                     </div>
-                    <div class="col col-10 text-left">
+                    <div class="col col-11 text-left">
                         {{$p->processo}}
                     </div>
                 </div><hr/>
                 <div class="row">
-                    <div class="col col-2">
+                    <div class="col col-1">
                     <strong>Data:</strong>
                     </div>
-                    <div class="col col-10 text-left">
+                    <div class="col col-11 text-left">
                         {{(new DateTime($p->data))->format('d/m/Y')}}
                     </div>
                 </div><hr/>
