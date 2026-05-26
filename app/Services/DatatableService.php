94dffb9dac1ccc7250b5a5108b46177575881c9e
Sostenes Junior <sostenes.dev@gmail.com>
2026-05-26 14:56:56 -0300

    ajuste nas buscas

--- app/Services/DatatableService.php
+++ app/Services/DatatableService.php
@@ -11,37 +11,43 @@ use Illuminate\Http\Request;
         return response()->json($response, 200);
     }
  */
-class DatatableService{
-    public static function getData(Model $model, string $searchParam, Request $request){
+
+class DatatableService
+{
+    public static function getData(Model $model, string $searchParam, Request $request)
+    {
         $start = $request['start'];
         $search = $request['search'];
         $draw = $request['draw'];
         $pageSize = 10;
         $search = $request['search']['value'];
-        if($search != null){
-            $result = $model::where($searchParam,'LIKE', '%'.$search.'%' )->skip($start)->take($pageSize)->get();
-        }else{
-            $result = $model::skip($start)->take($pageSize)->get();
+        if ($search != null) {
+            $result = $model::where($searchParam, 'LIKE', '%' . $search . '%')->skip($start)->take($pageSize)->get();
+        } else {
+            $result = $model::orderBy('id', 'desc')->skip($start)->take($pageSize)->get();
         }
         $recordsTotal = $model::count();
-        $response = (object)['data'=>$result, 'recordsTotal' => $recordsTotal, 'recordsFiltered' => $recordsTotal, $draw];
+        $response = (object)['data' => $result, 'recordsTotal' => $recordsTotal, 'recordsFiltered' => $recordsTotal, $draw];
         return $response;
     }
 
-    public static function getDataWithBoolFilter(Model $model, string $blFilter, $blValue="post", string $searchParam, Request $request){
+    public static function getDataWithBoolFilter(Model $model, string $blFilter, $blValue = "post", string $searchParam, Request $request)
+    {
         $start = $request['start'];
         $search = $request['search'];
         $draw = $request['draw'];
         $pageSize = 10;
         $search = $request['search']['value'];
-        $query = $model::where($blFilter,$blValue);
-        if($search != null){
-            $result = $query->where($searchParam,'LIKE', '%'.$search.'%' );
+        $query = $model::where($blFilter, $blValue);
+        if ($search != null) {
+            $result = $query->where($searchParam, 'LIKE', '%' . $search . '%');
+        } else {
+            $result = $query;
         }
-        
-        $result = $query->skip($start)->take($pageSize)->get();
+
+        $result = $result->orderBy('id', 'desc')->skip($start)->take($pageSize)->get();
         $recordsTotal = $model::count();
-        $response = (object)['data'=>$result, 'recordsTotal' => $recordsTotal, 'recordsFiltered' => $recordsTotal, $draw];
+        $response = (object)['data' => $result, 'recordsTotal' => $recordsTotal, 'recordsFiltered' => $recordsTotal, $draw];
         return $response;
     }
 }
